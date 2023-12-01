92c2e0675cc75b47b79146cd5f83599262629a22
Piotr Białas <96840701+BialasPiotr@users.noreply.github.com>
2023-12-01 10:52:37 +0100

    Day1
    My solution

---
+++ Day1/SnowCalibration/src/main/java/org/example/SnowCalibration.java
@@ -0,0 +1,43 @@
+package org.example;
+
+import java.io.InputStream;
+import java.util.Scanner;
+
+public class SnowCalibration {
+    public static void main(String[] args) {
+        try {
+            InputStream inputStream = SnowCalibration.class.getClassLoader().getResourceAsStream("input.txt");
+            if (inputStream == null) {
+                throw new IllegalArgumentException("File not found!");
+            } else {
+                Scanner scanner = new Scanner(inputStream);
+                int totalSum = 0;
+
+                while (scanner.hasNextLine()) {
+                    String line = scanner.nextLine();
+                    int firstDigit = -1, lastDigit = -1;
+
+                    for (int i = 0; i < line.length(); i++) {
+                        if (Character.isDigit(line.charAt(i))) {
+                            if (firstDigit == -1) {
+                                firstDigit = Character.getNumericValue(line.charAt(i));
+                            }
+                            lastDigit = Character.getNumericValue(line.charAt(i));
+                        }
+                    }
+
+                    if (firstDigit != -1 && lastDigit != -1) {
+                        int calibrationValue = firstDigit * 10 + lastDigit;
+                        totalSum += calibrationValue;
+                    }
+                }
+
+                System.out.println("Sum of all calibration values: " + totalSum);
+                scanner.close();
+            }
+        } catch (Exception e) {
+            System.out.println("Error");
+            e.printStackTrace();
+        }
+    }
+}
